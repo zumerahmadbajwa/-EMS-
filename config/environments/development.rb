ed13f122c29b08a9cb14897c7b3af0ca9fd2a456
Zumer Ahmad <macbookpro@Macbooks-MacBook-Pro.local>
2022-10-04 15:50:46 +0500

    devise implemented

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  #The generator will install an initializer which describes ALL of Devise's configuration options. It is imperative that you take a look at it. 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  #Now any email will pop up in your browser instead of being sent.
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
