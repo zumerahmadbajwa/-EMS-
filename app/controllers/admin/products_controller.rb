929f972895d49fbe42a066ef0e43721d0700ef86
Zumer Ahmad <macbookpro@Macbooks-MacBook-Pro.local>
2022-10-11 17:24:43 +0500

    feedback resolved

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -13,7 +13,6 @@ module Admin
             .page params[:page]
       respond_to do |format|
         format.html
-        format.csv { send_data @products.to_csv }
         format.xls { send_data @products.to_csv(col_sep: "\t") }
       end
     end
