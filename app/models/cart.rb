6be2a5ae83d3812154dc47d35a487f58e2e96fd3
Zumer Ahmad <macbookpro@Macbooks-MacBook-Pro-2.local>
2022-10-14 20:37:35 +0500

    deleted files merged

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,7 +3,7 @@
 # Cart model
 class Cart < ApplicationRecord
   has_many :order_items, dependent: :destroy
-  # has_many :products, through: :order_items
+  has_many :products, through: :order_items
 
   belongs_to :user
 
