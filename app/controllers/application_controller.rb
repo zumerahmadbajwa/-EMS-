4f7f5df407c1b4f008084c771f2a7743d8edd02a
Zumer Ahmad <macbookpro@Macbooks-MacBook-Pro-2.local>
2022-10-27 15:20:25 +0500

    profile feature added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :avatar) }
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
     devise_parameter_sanitizer.permit(:sign_in, keys: %i[login password])
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   # attr_accessor :login
   has_one :cart
   has_many :orders
+  has_one_attached :avatar
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
